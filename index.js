942485f76fec290c42d37c01a248dc1724016ef6
Sanket Lamsal <sanket.lamsal@gmail.com>
2020-08-13 15:04:20 +0000

    Modified for Repl.it

--- index.js
+++ index.js
@@ -4,59 +4,66 @@ const Discord = require("discord.js");
 const showAvatar = require("./avatar.js");
 const fs = require("fs");
 const nameList = require("./nameList.js");
-// const TOKEN = require("./token.js");
+const TOKEN = require("./token.js");
+const keepAlive = require('./server.js');
 
-(function () {
-  const data = fs.readFileSync("./scribbleNames.json");
-  let words = JSON.parse(data);
+let isIndeed, words;
+const PREFIX = "`";
+const client = new Discord.Client();
 
-  const bot = new Discord.Client();
+const token = TOKEN.token;
 
-  const token = process.env.TOKEN;
-  // const token = TOKEN.token;
+client.on("ready", () => {
+	isIndeed = false;
+	const data = fs.readFileSync("./scribbleNames.json");
+	words = JSON.parse(data);
+	console.log("Bot is online!");
+});
 
-  const PREFIX = "`";
-
-  bot.on("ready", () => {
-    console.log("Bot is online!");
-  });
-
-  bot.on("message", message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
-   if (
-      message.author.id === "726037232611491852" &&
-      ( message.content.toLowerCase() == "in-ded" ||
-       message.content.startsWith("inde")||
-       message.content.toLowerCase().startsWith("inde")||
-        message.content.toLowerCase() == "in-deed")
-    ) {
-      message.reply("Indeed you are an Idiot !!! :rofl:");
-    }
-    
-    if (message.content.startsWith(PREFIX)) {
-      switch (args[0]) {
-        case "baka":
-          displayBoka(args, message);
-          break;
-        case "avatar":
-          showAvatar(message);
-          break;
-        case "race":
-          startRace(args, message, bot);
-          break;
-        case "gn":
-          message.channel.send(`Good Night ${args[1]}`);
-          break;
-      }
-      if (
-        message.author.id == "551615059030179861" ||
-        message.author.id == "505368807037206558" ||
-        message.author.id == "539368618006413363"
-      ) {
-        nameList(args[0], words, message, fs);
-      }
-    }
-  });
-
-  bot.login(token);
-})();
+client.on("message", message => {
+	let args = message.content.substring(PREFIX.length).split(" ");
+	if (message.content.startsWith(PREFIX)) {
+		switch (args[0]) {
+			case "baka":
+				displayBoka(args, message);
+				break;
+			case "avatar":
+				showAvatar(message);
+				break;
+			case "race":
+				startRace(args, message, client);
+				break;
+			case "toggleI":
+				if (message.author.id == "539368618006413363") {
+					isIndeed = !isIndeed;
+					message.reply(isIndeed);
+				}
+				break;
+		}
+		if (
+			message.author.id == "551615059030179861" ||
+			message.author.id == "505368807037206558" ||
+			message.author.id == "539368618006413363"
+		) {
+			nameList(args[0], words, message, fs);
+		}
+	}
+	if (isIndeed && message.author.id == "726037232611491852") {
+		if (message.content.toLowerCase().includes("aaa")) {
+			message.channel.send(
+				"https://tenor.com/view/vyx-furry-aaaa-aaaaa-scream-gif-17575013"
+			);
+		}
+		if (
+			message.content.toLowerCase() == "in-ded" ||
+			message.content.startsWith("inde") ||
+			message.content.toLowerCase().startsWith("inde") ||
+			message.content.toLowerCase() == "in-deed" ||
+			message.content.toLowerCase() == "indid"
+		) {
+			message.reply("Indeed you are an Idiot !!! :rofl:");
+		}
+	}
+});
+client.login(token);
+keepAlive();
