2296f46cca1e20d9cad35b50d3c77ad98ccad28b
llsanketll <sanket.lamsal@gmail.com>
2020-08-18 21:54:38 +0545

    Refactoring Code

---
+++ commands/avatar.js
@@ -0,0 +1,28 @@
+module.exports = {
+  name: "avatar",
+  description: "To show avatar of user",
+
+  execute(message, args, fs) {
+    if (message.mentions && message.mentions.users.first()) {
+      const userEmbed = {
+        image: {
+          url: message.mentions.users
+            .first()
+            .displayAvatarURL({ dynamic: true, format: "png", size: 1024 }),
+        },
+      };
+      message.channel.send({ embed: userEmbed });
+    } else {
+      const userEmbed = {
+        image: {
+          url: message.author.displayAvatarURL({
+            dynamic: true,
+            format: "png",
+            size: 1024,
+          }),
+        },
+      };
+      message.channel.send({ embed: userEmbed });
+    }
+  },
+};
--- index.js
+++ index.js
@@ -1,96 +1,67 @@
-const displayBoka = require("./boka.js");
-const startRace = require("./race.js");
 const Discord = require("discord.js");
-const showAvatar = require("./avatar.js");
 const fs = require("fs");
-const nameList = require("./nameList.js");
 const keepAlive = require("./server.js");
-const addBaka = require("./addBaka.js");
-const showHelp = require("./showHelp.js");
-const toggleVariables = require("./toggle.js");
-require("dotenv").config();
-
-let gifs;
-let variables = {
-  isIndeed: true,
-  isAA: true,
-  isDaWoo: true,
-  isBee: true,
-};
+const { TOKEN, PREFIX, variables } = require("./config.json");
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-const PREFIX = "`";
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
 
-const token = process.env.TOKEN;
+commandFiles.forEach(file => {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+});
 
-client.on("ready", () => {
+client.on("ready", async () => {
   console.log("Bot is online!");
-  gifs = JSON.parse(fs.readFileSync("./gifs.json"));
+  client.user.setActivity("BOT Testing", { type: "WATCHING" });
 });
 
 client.on("message", message => {
   let args = message.content.substring(PREFIX.length).split(" ");
-  try {
-    if (message.content.startsWith(PREFIX)) {
-      switch (args[0]) {
-        case "baka":
-          displayBoka(args, message, gifs);
-          break;
-        case "abaka":
-          addBaka(args[1].trim(), message, gifs, fs);
-          break;
-        case "avatar":
-          showAvatar(message);
-          break;
-        case "help":
-          showHelp(args[1], message);
-          break;
-        case "race":
-          startRace(args, message, client);
-          break;
-        case "toggle":
-          variables = toggleVariables(args[1], message, variables);
-          break;
-        case "danceboi":
-          message.channel.send(
-            "https://media.giphy.com/media/TfKfqjt2i4GIM/giphy.gif"
-          );
-          break;
-      }
-      if (
-        message.author.id == "551615059030179861" ||
-        message.author.id == "505368807037206558" ||
-        message.author.id == "621977065712910336"
-      ) {
-        nameList(args[0], message, fs);
-      }
+  const command = args[0];
+
+  if (
+    message.author.id == "551615059030179861" ||
+    message.author.id == "505368807037206558" ||
+    message.author.id == "621977065712910336"
+  ) {
+    client.commands.get("Scribble").execute(message, args, fs);
+  }
+  if (variables.isBee && message.author.id == "667444572632121375") {
+    if (message.content == "yes ma'am") {
+      message.channel.reply(
+        "https://tenor.com/view/hug-virtual-hug-hug-sent-gif-5026057"
+      );
     }
-    if (variables.isBee && message.author.id == "667444572632121375") {
-      if (message.content == "yes ma'am") {
-        message.channel.reply(
-          "https://tenor.com/view/hug-virtual-hug-hug-sent-gif-5026057"
-        );
-      }
+  }
+  if (variables.isDaWoo && message.author.id == "539368618006413363") {
+    if (message.content.toLowerCase().startsWith("da")) {
+      const person = message.content.split(" ")[1];
+      message.channel.send(`Da ${person} run while you can he's gonna get you`);
     }
-    if (variables.isDaWoo && message.author.id == "539368618006413363") {
-      if (message.content.toLowerCase().startsWith("da")) {
-        const person = message.content.split(" ")[1];
-        message.channel.send(
-          `Da ${person} run while you can he's gonna get you`
-        );
-      }
+  }
+  if (message.author.id == "726037232611491852") {
+    if (variables.isAA && message.content.toLowerCase().includes("aaa")) {
+      message.channel.send(
+        "https://tenor.com/view/vyx-furry-aaaa-aaaaa-scream-gif-17575013"
+      );
     }
-    if (message.author.id == "726037232611491852") {
-      if (variables.isAA && message.content.toLowerCase().includes("aaa")) {
-        message.channel.send(
-          "https://tenor.com/view/vyx-furry-aaaa-aaaaa-scream-gif-17575013"
-        );
-      }
+  }
+
+  if (message.content.startsWith(PREFIX)) {
+    if (!client.commands.has(command)) return;
+
+    try {
+      client.commands.get(command).execute(message, args, fs);
+    } catch (err) {
+      console.log(err);
+      message.reply("There was an error executing the command");
     }
-  } catch (err) {
-    if (err) console.log(err);
   }
 });
 keepAlive();
-client.login(token);
+client.login(TOKEN);
